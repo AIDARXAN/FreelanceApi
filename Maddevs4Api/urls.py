ff6f566c8d1c00914ed14416b972528c19d0ac32
AIDARXAN <mainwinnertactics@gmail.com>
2019-07-18 08:19:42 +0600

    django-rest-auth installed

--- Maddevs4Api/urls.py
+++ Maddevs4Api/urls.py
@@ -17,6 +17,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('Snippet.urls')),
-    path('api-auth/', include('rest_framework.urls'))
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration', include('rest_auth.registration.urls')),
 ]
 
